8e14c9c162130c45bcade6aa57585ad592119783
jasmin <jasminmachuca8@gmail.com>
2020-06-02 20:37:50 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace ejercicios_exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programa dias de las semanas
+            int dia;
+            Console.WriteLine("ingrese un numero para saber el dia de semana");
+            dia = Convert.ToInt32(Console.ReadLine());
+            if (dia >0 && dia <=7)
+
+            {
+         
+                
+                if (dia == 1)
+                    Console.WriteLine("lunes");
+                if (dia == 2)
+                    Console.WriteLine("martes");
+                if (dia == 3)
+                    Console.WriteLine("miercoles");
+                if (dia == 4)
+                    Console.WriteLine("jueves");
+                if (dia == 5)
+                    Console.WriteLine("viernes");
+                if (dia == 6)
+                    Console.WriteLine("sabado");
+                if (dia == 7)
+                    Console.WriteLine("domingo");
+                sabado & domingo = Console.WriteLine("son fines de semana");
+               
+               
+            }
+            else
+            {
+                Console.WriteLine("Error la semana solo tiene 7 dias");
+               
+
+            }
+
+        }
+    }
+}
+
---
+++ ejercicio2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ejer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precioProducto, efec, tar, pag;
+
+            Console.WriteLine("ingrese el precio del producto a pagar");
+            precioProducto = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Diga que forma usted quiere pagar:");
+            Console.WriteLine("Efectivo marque 1");
+            Console.WriteLine("Tarjeta marque 2");
+            pag = Convert.ToDouble(Console.ReadLine());
+
+            if (pag == 1)
+            {
+                Console.WriteLine("Usted ha elegido el  pago en efectivo:");
+                Console.WriteLine("ingrese el monto");
+                efec = Convert.ToDouble(Console.ReadLine());
+                if (efec > 0)
+                {
+                    Console.WriteLine("El monto ingresado es positivo");
+                }
+                else { Console.WriteLine("El monto ingresado es negativo"); }
+            }
+            else if (pag == 2)
+            {
+
+                Console.WriteLine("Usted ha elegido el pago por tarjeta:");
+                Console.WriteLine("Digite el numero de la tarjeta");
+                tar = Convert.ToDouble(Console.ReadLine());
+
+
+                if ((tar >= 100000000000) && (tar <= 199999999999))
+                {
+                    ;
+                    Console.WriteLine("");
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Faltan dígitos o digitó de más");
+                }
+                if (tar > 0)
+                {
+
+                    Console.WriteLine("El pago es positivo");
+                }
+                else { Console.WriteLine("El pago es negativo"); }
+            }
+            else { Console.WriteLine("Error la opción que usted ingresó, no se encuentra disponible."); }
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
